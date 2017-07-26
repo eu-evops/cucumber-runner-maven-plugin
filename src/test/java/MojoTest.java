34f26bdbccf7b8dbe2ede1f5f7ab064361924618
Stanislaw Wozniak @ BA <stanislaw.wozniak@ba.com>
2017-07-26 14:08:10 +0100

    Fixing unit tests

--- src/test/java/MojoTest.java
+++ src/test/java/MojoTest.java
@@ -9,39 +9,27 @@ import static org.junit.Assert.assertTrue;
  * Created by n450777 on 20/10/2016.
  */
 public class MojoTest {
+    File testProjectDirectory = new File(".", "test-project").getAbsoluteFile();
 
     @Test
     public void  testStreamingCombinedHtmlFolderIsGeneratedWhenAThreadIsStopped() throws IOException, InterruptedException {
-        String resource = String.format("%s/test-project/pom.xml", new File(".").getAbsoluteFile().getParent());
-        System.out.println(resource);
-
         ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command("mvn", "clean", "integration-test", "-f", resource);
-
-        processBuilder.redirectError(new File("/tmp/mytest.err.log"));
-        processBuilder.redirectOutput(new File("/tmp/mytest.out.log"));
+        processBuilder.directory(testProjectDirectory);
+        processBuilder.command("mvn", "clean", "integration-test", "-DuseEnhancedJsonReporting=true");
 
         Process start = processBuilder.start();
         start.waitFor();
 
-        System.out.print("Validate Streaming Combined-html folder");
-        String cucumberPath = String.format("%s/test-project/target/cucumber/streaming-combined-html/cucumber-html-reports",
-                new File("").getAbsolutePath());
-        File cucumberHtmlReports = new File(cucumberPath);
+        File cucumberHtmlReports = new File(testProjectDirectory, "target/cucumber/combined-html/cucumber-html-reports");
         assertTrue("Streaming Combined Html Reports are not generated when all/one of the cucumber threads interrupted",
                 cucumberHtmlReports.exists());
     }
 
     @Test
     public void testCombinedHtmlFolderIsGeneratedWhenAThreadIsStopped() throws IOException, InterruptedException, NoSuchFieldException, IllegalAccessException {
-        String resource = String.format("%s/test-project/pom.xml", new File(".").getAbsoluteFile().getParent());
-        System.out.println(resource);
-
         ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command("mvn", "clean", "integration-test", "-f", resource,"-DcucumberRunner.streamingFormatter=false");
-
-        processBuilder.redirectError(new File("/tmp/mytest.err.log"));
-        processBuilder.redirectOutput(new File("/tmp/mytest.out.log"));
+        processBuilder.directory(testProjectDirectory);
+        processBuilder.command("mvn", "clean", "integration-test");
 
         Process start = processBuilder.start();
         start.waitFor();
